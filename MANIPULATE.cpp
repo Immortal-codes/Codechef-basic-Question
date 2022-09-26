57ff4bf852659580ba66a003ed5790bec941d7e5
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2022-09-26 14:02:53 +0530

    Add files via upload

---
+++ MANIPULATE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(y>x)
+	    {
+	        cout<<"No"<<endl;
+	    }
+	    else
+	    {
+	    cout<<"Yes"<<endl;
+	    }
+	}
+	return 0;
+}
